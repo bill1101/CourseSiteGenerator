248bf977df324c5178969799c98970e905a1c3f3
bill1101 <tyx@10.1.172.175>
2017-04-03 15:19:08 -0400

    Comment:Creating files

---
+++ src/csg/CSGeneratorApp.java
@@ -0,0 +1,25 @@
+package csg;
+
+import djf.AppTemplate;
+import java.util.Locale;
+import static javafx.application.Application.launch;
+
+/**
+ *
+ * @author tyx
+ */
+public class CSGeneratorApp extends AppTemplate{
+
+    @Override
+    public void buildAppComponentsHook() {
+        dataComponent = new CSData(this);
+        workspaceComponent = new CSWorkspace(this);
+        fileComponent = new CSFiles(this);
+        styleComponent = new CSStyle(this);
+    }
+    
+    public static void main(String[] args) {
+	Locale.setDefault(Locale.US);
+	launch(args);
+    }
+}
